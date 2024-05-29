b64905a82c411b7fc85256142c5453b5fea8296b
SitrakaRasata45 <rasatasitraka2@gmail.com>
2024-05-29 20:41:14 +0300

    Update apiServer.config.js

--- src/config/apiServer.config.js
+++ src/config/apiServer.config.js
@@ -3,7 +3,7 @@ import env from 'env-var';
 
 const PORT = env.get('API_PORT').required().asIntPositive();
 const DEV_MODE = env.get('API_ENV').asString() === 'development';
-const API_HOST = env.get('API_HOST').required().asString();
+const API_HOST = env.get('API_HOST').asString();
 const API_SECRET_KEY = env.get('API_SECRET_KEY').required().asString();
 const API_TOKEN_EXPIRATION = env.get('API_TOKEN_EXPIRATION').required().asString();
 
